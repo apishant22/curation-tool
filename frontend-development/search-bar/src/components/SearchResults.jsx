73fe8912ac40940f7bfba7f5abb5b5f14fe2ce4e
Danial Akmal <danialdakmal@gmail.com>
2024-10-17 21:53:39 +0100

    Add ORCID ID and added the router dom for page navigation in the future

--- frontend-development/search-bar/src/components/SearchResults.jsx
+++ frontend-development/search-bar/src/components/SearchResults.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 const SearchResults = ({ results }) => {
+
+  const handleClick = (user) => {
+    console.log(user.name);
+  }
+
   return (
     <div className='flex items-center justify-center'>
       <div className={`${results && results.length > 0 ? 'flex' : 'hidden'} bg-white w-[300px] md:w-[400px] flex-col shadow-lg rounded-lg mt-2 px-3 py-2`}>
         {results.map((result, idx) => (
-          <div key={idx}>
-            <p className='cursor-pointer'>{result.name}</p>
-          </div>
+          <button className='cursor-pointer hover:bg-blue-50 border-b border-gray-300 text-left' key={idx} onClick={() => handleClick(result)}>
+            <p>{result.name}</p>
+            <p className='text-sm'>ORCID ID: {result.address.zipcode}</p>
+          </button>
         ))}
       </div>
     </div>
