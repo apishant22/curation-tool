bbbe1fe0af8a8f29eee7b9e54d4037bf74b91b2c
Danial Akmal <danialdakmal@gmail.com>
2024-10-23 09:49:49 +0100

    Add navigation functionality when you click the result
    Now if you click the result, it will navigate to the /summary page and it should display the author's summary and papers. But it is still in progress so I will do it by today or tomorrow.

--- frontend-production/src/components/ResultCard.jsx
+++ frontend-production/src/components/ResultCard.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const ResultCard = () => {
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/summary');
+  }
   return (
-    <div className='bg-blue-200/75 p-6 rounded-xl hover:cursor-pointer'>
+    <div>
+      <button className='bg-blue-200/75 p-6 rounded-xl hover:cursor-pointer w-full' onClick={handleClick}>
       {/* fetch result and populate inside the card */}
-      <div className=''>
+      <div className='text-left'>
       <h1>Name here</h1>
       <p>Orcid ID here</p>
       <p>Employment here</p>
       </div>
 
+      </button>
     </div>
+
   )
 }
 
--- frontend-production/src/components/Summary.jsx
+++ frontend-production/src/components/Summary.jsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom'
 import Event from './Event';
 import Button from './Button';
 import Footer from './Footer';
+import Header from './Header';
 
 const Summary = () => {
 
@@ -13,39 +14,14 @@ const Summary = () => {
   const {searchQuery} = location.state || {};
   console.log(searchQuery);
 
-  if (!searchQuery) {
-    return <div>No user available</div>
-  }
+
 
 
   return (
     <div className='flex flex-col justify-between h-screen overflow-auto'>
-      <div className='flex flex-grow'>
-      <div className='w-[60%] flex flex-col'>
-        {/* should fetch the name and summary from API here */}
-        <h1 className='name text-center p-4'>Adriana Wilde</h1>
-        <p className='summary p-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa assumenda earum ut, voluptatibus omnis sit similique pariatur cum a corporis doloribus, voluptates praesentium ea. Quos harum possimus minima laborum fuga.</p>
-        <div className='bg-gray-400 p-4 flex-1'>
-          <div className='bg-gray-600'>Summary of works</div>
-          <div className='bg-gray-600'>Paper</div>
-          <div className='bg-gray-600'>Paper</div>
-        </div>
-        <div className='flex justify-evenly mt-5'>
-          <Button text={'Accept'}/>
-          <Button text={'Regenerate'}/>
-          <Button text={'Back'}/>
-        </div>
-      </div>
-      <div className='flex-1'> 
-        <h1 className='text-center'>
-          Timeline
-        </h1>
-        {/* should do some mapping here */}
-        <Event time={time} events={events}/>
-        <Event time={time} events={events}/>
-        <Event time={time} events={events}/>
-        <Event time={time} events={events}/>
-      </div>
+      <Header/>
+      <div className='text-center'>
+        summary here
       </div>
       <Footer/>  
     </div>
