30227f3f5834c8134efea365f275816c6212ca11
Bethany Ahluwalia <batahluwalia@gmail.com>
2024-11-07 12:08:01 +0000

    Added initial files for LLM web search

---
+++ backend/llm/config/__init__.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+azure_openai_endpoint = os.getenv('AZURE_OPENAI_ENDPOINT')
+azure_openai_key_key = os.getenv('AZURE_OPENAI_KEY')
+azure_api_version = os.getenv('AZURE_API_VERSION', '2023-07-01-preview')
+azure_openai_deployment_name = os.getenv('AZURE_OPENAI_DEPLOYMENT_NAME')
+serpapi_key = os.getenv("SERPAPI_KEY")
