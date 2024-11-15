b2122eae9e46d8571b8ff8fbd6239c182844da77
Winston2g20 <72627441+Winston2g20@users.noreply.github.com>
2024-11-15 16:07:36 +0000

    JSON code done :-)

--- backend/authorNetworkCode.py
+++ backend/authorNetworkCode.py
@@ -4,71 +4,85 @@ import json
 import requests
 import base64
 from backend.db import db_helper
-from backend.db.models import Researcher, Paper, Paper_Authors, Organisation, Dept, Researcher_Employment, Researcher_Edu
+from backend.db.models import PaperAuthors,Researcher
 from sqlalchemy import not_
 
 def retrieve_formatted_records(model, filters=None):
     session = db_helper.get_session()
-
     query = session.query(model)
-
     if filters:
         query = query.filter_by(**filters)
 
     results = query.all()
     return [{key: str(value) for key, value in result.__dict__.items() if not key.startswith('_')}
                 for result in results]
-def orcid_to_name(orcidID):
-    return db_helper.get_author_details_from_db(orcidID).get("Name")
-def generatesNodesAndEdges(orcid):
 
 
-    var = db_helper.get_author_details_from_db(orcid).get("Publications")
-    paper_dois = []
-    collaborators = []
-    occurrence_dict = {}
+
+def generatesNodesAndEdges(id):
+    var = retrieve_formatted_records(PaperAuthors,{'id': id})
     nodes = []
     edges = []
-    researcher = orcid_to_name(orcid)
+    relevantdois = []
+    ids = []
+    idset = []
+
+    for record in var:
+        relevantdois.append(record.get('doi'))
+
+    for doi in relevantdois:
+        collaborators = []
+        for entry in retrieve_formatted_records(PaperAuthors,{'doi': doi}):
+            collaborators.append(entry.get('id')) if entry.get('id') != str(id) else None
+        ids.append(collaborators)
+
+    for element in ids:
+        for number in element:
+            if number not in idset:
+                idset.append(number)
+
+    temp = retrieve_formatted_records(Researcher, {'id': id})[0]
+    nodes.append({"id":temp.get("id"),"name":temp.get("name"),"link":temp.get("profile_link")})
+    for item in idset:
+        record = retrieve_formatted_records(Researcher,{'id':int(item)})[0]
+        nodes.append({"id":record.get("id"),"name":record.get("name"),"link":record.get("profile_link")})
 
 
-    for i in range(len(var)):
-        paper_dois.append(var[i].get("DOI"))
 
-    for doi in paper_dois:
-        authorList = []
-        coauthors = []
-        collaborators_dict_list = retrieve_formatted_records(Paper_Authors,filters={"doi":doi})
-        for i in collaborators_dict_list:
-            number = i.get('orcid')
-            coauthors.append(number) if number != orcid else None
-        collaborators.append(coauthors)
+    for lists in ids:
+        for person1 in lists:
+            for person2 in lists:
+                if person1 != person2 and {"source":person1,"target":person2} not in edges and {"source":person2,"target":person1} not in edges:
+                    edges.append({"source":person1,"target":person2})
 
-    for i in range(len(collaborators)):
-        for orcidID in collaborators[i]:
-            name = db_helper.get_author_details_from_db(orcidID).get("Name")
-            if(name in occurrence_dict):
-                occurrence_dict[name] += 1
-            else:
+    for lists in ids:
+        for person1 in lists:
+            edges.append({"source": temp.get("id"), "target": person1})
 
-                occurrence_dict[name] = 1
-
-
-    nodes = list(occurrence_dict.keys())
-    newList = []
-    for lists in collaborators:
-        newList.append(list(map(orcid_to_name, lists))) if lists != [] else None
-
-    for i in range(len(newList)):
-        for item1 in newList[i]:
-            for item2 in newList[i]:
-                edges.append((item1,item2)) if item1 != item2 and (item1,item2) not in edges and (item2,item1) not in edges else None
-
-    nodes.append(researcher)
-    for item in nodes:
-        edges.append((researcher,item))
 
     return nodes, edges
+
+
+
+def convert_to_json(id):
+    nodes,edges = generatesNodesAndEdges(id)
+
+    graph_data = {
+        "nodes":nodes,
+        "edges":edges
+    }
+    json_string = json.dumps(graph_data, indent=4)
+    print(json_string)
+
+    with open("graph.json", "w") as json_file:
+        json.dump(graph_data, json_file, indent=4)
+
+
+
+
+
+
+
 
 
 
