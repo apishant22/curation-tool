357c6244472ddc2981324c0109eedd254c5ef8f6
Bethany Ahluwalia <batahluwalia@gmail.com>
2024-11-15 12:52:13 +0000

    Updated llmNew.py and system_prompt.txt llmNew.request now takes author name instead of orcid_id All author details are passed to LLM instead of just name

--- backend/llm/llmNew.py
+++ backend/llm/llmNew.py
@@ -2,15 +2,13 @@ import os
 from datetime import datetime
 from pydantic import BaseModel
 from typing import List
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 from backend.llm.core.azure_functions import AzureOpenAIFunctions
 import backend.llm.config as config
 import backend.llm.functions.web_browsing as browser
-
-import sys
-
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from backend.db import db_helper
 
 # -- The message schema for the assistant
@@ -56,29 +54,27 @@ def load_prompt(prompt):
     except FileNotFoundError:
         print(f"Error: The file 'prompt.txt' was not found at {prompt_path}")
         return "Default prompt text or handle the error appropriately."
+    
 
-
-def request(orcid_id):
-    subject = db_helper.get_author_details_from_db(orcid_id)
-    name = subject['Name']
+def request(author_name):
+    author = db_helper.get_author_details_from_db(author_name)
+    if author is None:
+        return {"reply": "Author not found in the database."}
     conversation = Conversation(conversation=[])
     system_message = Message(role='system', content=load_prompt('system_prompt.txt'))
     conversation.conversation.insert(0, system_message)
-    prompt_message = Message(role='user', content=name)
+    prompt_message = Message(role='user', content=str(author))
     conversation.conversation.insert(1, prompt_message)
     conversation_dict = [message.model_dump() for message in conversation.conversation]
     response = assistant.ask(conversation_dict)
     summary = response.choices[0].message.content
     log(response)
-    #RED = '\033[91m'
-    #GREEN = '\033[92m'
-    #ENDC = '\033[0m'  # Resets the color to default
-    #print(f"{RED}\nQuery: {prompt_message} {ENDC}\n")
-    #print(f"{GREEN}Reply: {summary}{ENDC}\n")
-    #print(f"{RED}\nOutput: {response} {ENDC}\n")
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    ENDC = '\033[0m'  # Resets the color to default
+    print(f"{RED}\nQuery: {prompt_message} {ENDC}\n")
+    print(f"{GREEN}Reply: {summary}{ENDC}\n")
+    print(f"{RED}\nOutput: {response} {ENDC}\n")
     #return {"reply": response.choices[0].message.content}
-    db_helper.update_researcher_summary(orcid_id, summary)
+    db_helper.update_researcher_summary(author_name, summary)
     #print(f"{RED}{db_helper.get_researcher_summary(orcid_id, session=None)}")
-
-
-#request("0000-0002-1684-1539")
