7a48eae9f871af1cb47c119a2dcc66cca17f8077
Danial Akmal <danialdakmal@gmail.com>
2024-11-21 11:46:41 +0000

    Add UI enhancement to search bar

--- frontend-v2/components/navbar/Search.tsx
+++ frontend-v2/components/navbar/Search.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect, useRef} from "react"
+import { useEffect, useRef } from "react";
 import React, { useCallback, useState } from "react";
 import { BiSearch } from "react-icons/bi";
 import { FaUserGraduate } from "react-icons/fa";
@@ -20,7 +20,7 @@ const Search = () => {
   const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
   const [category, setCategory] = useState("Author");
-  const [placeholder, setPlaceholder] = useState("Search");
+  const [placeholder, setPlaceholder] = useState("Search an author ...");
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   // get the value from the search bar
@@ -66,7 +66,9 @@ const Search = () => {
     }
   };
 
-  const handleSubmit = async (e: React.FormEvent | React.KeyboardEvent<HTMLInputElement>) => {
+  const handleSubmit = async (
+    e: React.FormEvent | React.KeyboardEvent<HTMLInputElement>
+  ) => {
     e.preventDefault();
     if (!input.trim()) {
       return;
@@ -95,18 +97,21 @@ const Search = () => {
       handleSubmit(e);
     }
   };
-  
+
   useEffect(() => {
-    const handleClickOutside = (e: MouseEvent) =>{
-      if(dropdownRef.current && !dropdownRef.current.contains(e.target as Node)){
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(e.target as Node)
+      ) {
         setIsOpen(false);
       }
     };
-    document.addEventListener("mousedown",handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener("mousedown",handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
-  },[]);
+  }, []);
 
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
@@ -142,7 +147,7 @@ const Search = () => {
                 onClick={() =>
                   handleCategoryClick("Author", "Search author ...")
                 }>
-                <div className="flex items-center p-1 transition duration-200 bg-white hover:bg-zinc-100 dark:hover:bg-zinc-950 rounded-lg">
+                <div className="flex items-center p-1 transition duration-200 bg-white hover:bg-zinc-100 dark:bg-zinc-900 dark:hover:bg-zinc-950 rounded-lg">
                   <div className="p-1">
                     <FaUserGraduate />
                   </div>
@@ -158,7 +163,7 @@ const Search = () => {
                     "Search research field ..."
                   )
                 }>
-                <div className="flex items-center p-1 transition duration-200 bg-white hover:bg-zinc-100 dark:hover:bg-zinc-950 rounded-lg">
+                <div className="flex items-center p-1 transition duration-200 bg-white hover:bg-zinc-100 dark:bg-zinc-900 dark:hover:bg-zinc-950 rounded-lg">
                   <div className="p-1">
                     <IoBookSharp />
                   </div>
@@ -170,7 +175,7 @@ const Search = () => {
         </div>
       </div>
       <input
-        className="ml-2 flex-1 font-sans text-xs outline-none md:text-base border-none focus:ring-0"
+        className="ml-2 flex-1 font-sans text-xs outline-none md:text-base border-none focus:ring-0 dark:bg-zinc-800"
         type="search"
         placeholder={placeholder}
         value={input}
@@ -184,7 +189,29 @@ const Search = () => {
         disabled={loading}
         className="pr-2 flex items-center justify-center hover:text-blue-500">
         {loading ? (
-          <div className="animate-spin h-5 w-5 border-2 border-gray-500" />
+          <div className="grid w-full place-items-center overflow-x-scroll rounded-lg lg:overflow-visible">
+            <svg
+              className="text-gray-300 animate-spin"
+              viewBox="0 0 64 64"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24">
+              <path
+                d="M32 3C35.8083 3 39.5794 3.75011 43.0978 5.20749C46.6163 6.66488 49.8132 8.80101 52.5061 11.4939C55.199 14.1868 57.3351 17.3837 58.7925 20.9022C60.2499 24.4206 61 28.1917 61 32C61 35.8083 60.2499 39.5794 58.7925 43.0978C57.3351 46.6163 55.199 49.8132 52.5061 52.5061C49.8132 55.199 46.6163 57.3351 43.0978 58.7925C39.5794 60.2499 35.8083 61 32 61C28.1917 61 24.4206 60.2499 20.9022 58.7925C17.3837 57.3351 14.1868 55.199 11.4939 52.5061C8.801 49.8132 6.66487 46.6163 5.20749 43.0978C3.7501 39.5794 3 35.8083 3 32C3 28.1917 3.75011 24.4206 5.2075 20.9022C6.66489 17.3837 8.80101 14.1868 11.4939 11.4939C14.1868 8.80099 17.3838 6.66487 20.9022 5.20749C24.4206 3.7501 28.1917 3 32 3L32 3Z"
+                stroke="currentColor"
+                strokeWidth="5"
+                strokeLinecap="round"
+                strokeLinejoin="round"></path>
+              <path
+                d="M32 3C36.5778 3 41.0906 4.08374 45.1692 6.16256C49.2477 8.24138 52.7762 11.2562 55.466 14.9605C58.1558 18.6647 59.9304 22.9531 60.6448 27.4748C61.3591 31.9965 60.9928 36.6232 59.5759 40.9762"
+                stroke="currentColor"
+                strokeWidth="5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="text-gray-900 dark:text-white"></path>
+            </svg>
+          </div>
         ) : (
           <BiSearch size={25} />
         )}
