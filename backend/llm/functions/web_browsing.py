b7bf53b7978dd356a2e1243e3fd1e7ef7ca9fcb4
Bethany Ahluwalia <batahluwalia@gmail.com>
2024-12-03 14:18:30 +0000

    Cleaned up unused code

--- backend/llm/functions/web_browsing.py
+++ backend/llm/functions/web_browsing.py
@@ -1,11 +1,9 @@
 import json
 
 from functions.duck_duck_go_search import DuckDuckGoSearchManager
-#from functions.google_search import GoogleSearchManager
 from functions.web_scraper import WebContentScraper
 
 ddg = DuckDuckGoSearchManager()
-#gs = GoogleSearchManager()
 scraper = WebContentScraper()
 
 
@@ -50,51 +48,6 @@ def news_search(query, num_results=3):
     return scraped_data
 
 
-def images_search(query, num_results=3):
-    """Performs the image search for a specific query. For example, "puppies". If possible, the output should be in Markdown format.
-
-    This function enables real-time image search and information retrieval for GPT models. It fetches relevant data from the internet in response to user queries, enhancing GPT's knowledge base.
-
-    :param query: The search query string for the image search.
-    :param num_results: The maximum number of URLs to return. Defaults to 3 if not provided. (optional)
-
-    :return: A list of dictionaries, where each dictionary contains 'image' (URL of the actual image) and 'thumbnail' (URL of the image's thumbnail).
-    """
-
-    image_info = ddg.images_search(query, int(num_results))
-    return image_info
-
-
-def videos_search(query, num_results=3):
-    """Performs the video for a specific query. For example, "video tutorial for Excel pivot table". If possible, the output should be in Markdown format.
-
-    This function enables real-time video search and information retrieval for GPT models. It fetches relevant data from the internet in response to user queries, enhancing GPT's knowledge base.
-
-    :param query: The search query string for finding relevant video results.
-    :param num_results: The maximum number of URLs to return. Defaults to 3 if not provided. (optional)
-
-    :return: A list of dictionaries, where each dictionary represents a search result. Each dictionary contains two keys: 'title', title of the content, and 'content', URL to the resource.
-    """
-
-    video_info = ddg.videos_search(query, int(num_results))
-    return video_info
-
-
-def maps_search(query, place, num_results=3):
-    """Performs the location for a specific query. For example, "Italian restaurant in Berlin". If possible, the output should be in Markdown format.
-
-    This function enables real-time location search and information retrieval for GPT models. It fetches relevant data from the internet in response to user queries, enhancing GPT's knowledge base.
-
-    :param query: The search query string for finding relevant location results.
-    :param place: The place where the search is performed.
-    :param num_results: The maximum number of URLs to return. Defaults to 3 if not provided. (optional)
-
-    :return: A list of dictionaries, each representing a restaurant. Each dictionary includes the location's title, address, phone number, URL, and a nested dictionary of operating hours with keys indicating days of the week and additional status information like 'closes_soon', 'is_open', and 'state_switch_time'.
-    """
-    map_info = ddg.maps_search(query, place, int(num_results))
-    return map_info
-
-
 def webpage_scraper(url):
     """Scrape a webpage for its text content.
 
@@ -109,16 +62,3 @@ def webpage_scraper(url):
         return result
     except Exception as e:
         return json.dumps({"error": str(e)})
-
-
-# Debug code
-# print(text_search("Is Sam Altman fired from OpenAI?", 5))
-# print(news_search("Is Sam Altman fired from OpenAI?", 5))
-# print(images_search("puppies", 5))
-# print(videos_search("video tutorial for Excel pivot table", 5))
-# print(maps_search("Italian  restaurant", "berlin", 5))
-# print(webpage_scraper("https://www.bbc.com/news/technology-67514068"))
-
-# from core.parser import FunctionDefinitionParser
-# parser = FunctionDefinitionParser()
-# print(parser.convert_function_to_json_schema(text_search))
--- backend/llm/main.py
+++ backend/llm/main.py
@@ -18,16 +18,11 @@ class Message(BaseModel):
 class Conversation(BaseModel):
     conversation: List[Message]
 
-system_prompt = """You are an AI assistant with access to websearch, Argocd, and weather functions.
+system_prompt = """You are an AI assistant with access to a websearch function.
 
 The websearch function empowers you for real-time web search and information retrieval, particularly for current and 
 relevant data from the internet in response to user queries, especially when such information is beyond your training 
 data or when up-to-date information is essential. Always include the source URL for information fetched from the web.
-
-The Argocd function facilitates real-time interaction with the Argocd instance, enabling you to fetch information about 
-applications deployed in the cluster. 
-
-The weather function provides real-time capabilities to offer current weather updates. 
 
 All your responses should be in a human-readable format.
 """
@@ -41,9 +36,6 @@ assistant = AzureOpenAIFunctions(
     functions=[
         browser.text_search,
         browser.news_search,
-        browser.images_search,
-        browser.videos_search,
-        browser.maps_search,
         browser.webpage_scraper
     ]
 )
@@ -54,58 +46,3 @@ async def endpoint(conversation_id: str, conversation: Conversation):
     conversation_dict = [message.model_dump() for message in conversation.conversation]
     response = assistant.ask(conversation_dict)
     return {"id": conversation_id, "reply": response.choices[0].message.content}
-
-
-# # -- Test the assistant. This is not part of the FastAPI app, only for demonstration purposes.
-# if __name__ == "__main__":
-#     prompt = "Is Sam Altman fired from OpenAI?"
-#     response = assistant.ask([{'role': 'user', 'content': f'{prompt}'}])
-#     # ANSI escape sequences for colors
-#     RED = '\033[91m'
-#     GREEN = '\033[92m'
-#     ENDC = '\033[0m'  # Resets the color to default
-#     print(f"{RED}\nQuery: {prompt} {ENDC}\n")
-#     print(f"{GREEN}Reply: {response.choices[0].message.content}{ENDC}")
-
-# Features:
-# -- Web search
-# -- Video search
-# -- Location search
-# -- Image search
-# -- Summarize article from URL
-# -- Weather information
-# -- Integration with ArgoCD application through rest API.
-
-# === Questions ===
-# ---- General search to recent events ----
-# -- Is Sam Altman fired from OpenAI?
-# -- What happened to HSBC bank in UK?
-# -- What happened to WeWork?
-
-# ---- About a person ----
-# -- Who is Frank Gotthard?
-
-# ---- Video search ----
-# -- Provide video tutorial for Excel pivot table.
-# -- Provide video tutorial for Python pandas.
-# -- Show me a video about how to make a cake.
-
-# ---- Location search ----
-# -- Suggestions for the top 3 Italian restaurant in Munich.
-
-# ---- Image search ----
-# -- Provide puppies images.
-# -- Provide 10 images of cats.
-# -- Show me pictures of the Eiffel Tower.
-# -- Show me pictures of the Eiffel Tower at night.
-
-# ---- Weather ----
-# -- What is the weather in Berlin today?
-# -- Is there any possibility of rain in Berlin today?
-
-# ---- Summarize from URL ----
-# -- Summarize the article in 3 sentences https://www.bbc.com/news/world-us-canada-67482231
-
-# ---- External application ----
-# -- How many argocd applications are available?
-# -- How many argocd applications are available? And what are their status?
