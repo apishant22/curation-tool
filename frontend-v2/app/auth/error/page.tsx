5f8d849547dd3be9b29eba07c77d47a9140e9e3c
Danial Akmal <danialdakmal@gmail.com>
2024-12-04 16:52:41 +0000

    Add UI improvements on login page and error login page

--- frontend-v2/app/auth/error/page.tsx
+++ frontend-v2/app/auth/error/page.tsx
@@ -1,22 +1,53 @@
 "use client";
 import Button from "@/components/global/Button";
 import Container from "@/components/global/Container";
+import Logo from "@/components/navbar/Logo";
 import { signIn } from "next-auth/react";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { MdError } from "react-icons/md";
+import background from "@/public/images/geometric.jpg";
+import { IoArrowBack } from "react-icons/io5";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 const SignInPage = () => {
   return (
-    <div className="flex justify-center items-center h-full">
+    <div
+      className="flex justify-center items-center h-full"
+      style={{
+        backgroundImage: `url(${background.src})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        width: "100vw",
+        height: "100vh",
+      }}>
       <Container>
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col gap-3 items-center justify-center">
-            <MdError size={50} className="text-red-700" />
-            <div className="text-center font-bold text-3xl">Access Denied</div>
-          </div>
+        <div className="w-[500px] h-fit flex flex-col items-center justify-center gap-4 p-4 bg-white rounded-xl">
           <div>
-            Please contact verified ACM-W members to have access to this tool.
+            <Logo />
+          </div>
+          <hr className="w-full" />
+
+          <div className="flex flex-col gap-4">
+            <div className="flex gap-3 items-center justify-center">
+              <MdError size={50} className="text-red-700" />
+              <div className="text-center font-bold text-3xl">
+                Access Denied
+              </div>
+            </div>
+            <div className="text-sm text-neutral-600">
+              Please contact whitelisted ACM-W members to have access to this
+              tool.
+            </div>
+            <div>
+              <Link href="/auth/login">
+                <IoArrowBack
+                  size={20}
+                  className="hover:text-neutral-500 transition duration-150"
+                />
+              </Link>
+            </div>
           </div>
         </div>
       </Container>
--- frontend-v2/app/auth/login/page.tsx
+++ frontend-v2/app/auth/login/page.tsx
@@ -5,6 +5,7 @@ import { signIn } from "next-auth/react";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import background from "@/public/images/geometric.jpg";
+import Logo from "@/components/navbar/Logo";
 
 const SignInPage = () => {
   return (
@@ -19,24 +20,29 @@ const SignInPage = () => {
       }}>
       <Container>
         <div className="w-[450px] h-fit flex flex-col items-center justify-center gap-4 p-4 bg-white rounded-xl">
-          <div className="text-center font-bold ">Login</div>
-          <div className="text-neutral-700 text-sm">
-            To access this tool, you must log in with your whitelisted email.
+          <div>
+            <Logo />
           </div>
-          <Button
-            outline
-            label="Log in with Google"
-            icon={FcGoogle}
-            onClick={() => {
-              return signIn("google", {
-                callbackUrl: "http://localhost:3000/",
-              });
-            }}
-          />
+          <hr className="w-full" />
 
-          <div className="text-neutral-700 text-sm">
-            If your email is not whitelisted yet, please contact the appropriate
-            ACM-W member to gain access.
+          <div className="py-4 flex flex-col gap-4">
+            <div className="text-neutral-700 text-sm">
+              To access this tool, you must log in with your whitelisted email.
+            </div>
+            <Button
+              outline
+              label="Login"
+              icon={FcGoogle}
+              onClick={() => {
+                return signIn("google", {
+                  callbackUrl: "http://localhost:3000/",
+                });
+              }}
+            />
+            <div className="text-neutral-700 text-sm">
+              If your email is not whitelisted yet, please contact the
+              appropriate ACM-W member to gain access.
+            </div>
           </div>
         </div>
       </Container>
